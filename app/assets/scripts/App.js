ad2429456a8212e8479b8ac4d112d7420cc95fbb
= <=>
2020-03-22 21:33:25 -0400

    added the entrypoint folder for webpack

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,14 @@
+// importing all the css
+import '../styles/styles.css';
+
+
+// importing react
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import created react components an example
+// import ExampleComponent from './modules/ExampleComponent';
+
+// ReactDOM.render(<ExampleComponent />, document.querySelector('element'))
+
+
